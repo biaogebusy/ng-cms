6f450ac22b4db879117dc2920ab17e88447456be
biaogebusy <349255833@qq.com>
2019-05-11 18:25:11 +0800

    refactor: core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -8,6 +8,8 @@ import { MenuComponent } from './menu/menu.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+
+import { MatIconRegistry } from '@angular/material';
 @NgModule({
   declarations: [
     MenuComponent,
@@ -15,11 +17,34 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent,
     FooterComponent
   ],
-  imports: [
-    CommonModule,
-    BrowserAnimationsModule,
+  imports: [CommonModule, BrowserAnimationsModule, SharedModule],
+  exports: [
+    MenuComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
     SharedModule
-  ],
-  exports: [MenuComponent, HeaderComponent, SidebarComponent, FooterComponent, SharedModule]
+  ]
 })
-export class CoreModule {}
+export class CoreModule {
+  /**
+   * @SkipSelf 让模块去父级寻找依赖，不然会造成死循环
+   * @Optional 可选，如果CoreModule不存在正常执行
+   * @param parent
+   * @param iconRegistry
+   * @param ds
+   */
+  constructor(
+    @Optional() @SkipSelf() parent: CoreModule
+    // TODO
+    // iconRegistry: MatIconRegistry,
+    // ds: DomSanitizer
+  ) {
+    if (parent) {
+      throw new Error('Core 模块已经存在，不能再加载！');
+    }
+    // @ts-ignore
+    // TODO
+    // loadSvgResources(iconRegistry, ds);
+  }
+}
