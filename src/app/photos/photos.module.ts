b977f574af872acd8cb17537896c66e98684a58c
biaogebusy <349255833@qq.com>
2019-05-11 23:32:29 +0800

    quick fix

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
   declarations: [
-    PhotosModule,
+    PhotoComponent,
     PhotoListComponent
   ],
   imports: [
