e4f5f4b647e2eea58ac5d93c2cb36d5a36ac8142
biaogebusy <349255833@qq.com>
2019-05-11 17:57:01 +0800

    feat: add shared module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,15 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
+import { DomSanitizer } from '@angular/platform-browser';
 
+import { SharedModule } from '../shared/shared.module';
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserAnimationsModule,
+    SharedModule
   ]
 })
 export class CoreModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatInputModule,
+  MatToolbarModule,
+  MatListModule
+} from '@angular/material';
+
+import { MatGridListModule } from '@angular/material/grid-list';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+/**
+ * 共享模块主要是导入其他模块需要的依赖，导入和导出
+ * 其他模块需要这些共享模块，只要导入share模块就好
+ */
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    FlexLayoutModule,
+    MatGridListModule
+  ],
+  exports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    FlexLayoutModule,
+    MatGridListModule
+  ]
+})
+export class SharedModule {}
