a5584992e61fe4ac0727613fd76cbc0ed9af7969
biaogebusy <349255833@qq.com>
2019-05-11 12:18:21 +0800

    feat: add interface

---
+++ src/app/inerfaces/article.ts
@@ -0,0 +1,9 @@
+export interface Article {
+    id: number; // 期数
+    title: string; // 文章标题
+    author: string; // 文章作者
+    url: string; // 原文地址
+    desc: string; // 文章描述
+    star: number; // 推荐指数
+    categories: Array<string>; // 文章分类
+}
--- src/app/inerfaces/photo.ts
+++ src/app/inerfaces/photo.ts
@@ -1,25 +1,25 @@
-mport { IPhotoUrl } from './photoUrl';
+import { IPhotoUrl } from './photoUrl';
 import { IUser } from './user';
 
 export interface IPhoto {
-    categories: Array<any>,
-    color: string,
-    created_at: Date,
-    current_user_collections: Array<any>,
-    description: string,
-    exif: Object,
-    height: number,
-    id: string,
-    like_by_user: boolean,
-    likes: number,
-    links: Object,
-    slug: string,
-    sponsored: boolean,
-    sponsored_by: string,
-    sponsored_impressions_id: string,
-    updated_at: Date,
-    urls: IPhotoUrl,
-    user: IUser,
-    views: number,
-    width: number
+  categories: Array<any>;
+  color: string;
+  created_at: Date;
+  current_user_collections: Array<any>;
+  description: string;
+  exif: Object;
+  height: number;
+  id: string;
+  like_by_user: boolean;
+  likes: number;
+  links: Object;
+  slug: string;
+  sponsored: boolean;
+  sponsored_by: string;
+  sponsored_impressions_id: string;
+  updated_at: Date;
+  urls: IPhotoUrl;
+  user: IUser;
+  views: number;
+  width: number;
 }
